3e61712e13efb04d9cdf2d0e156bce10afc763d3
Dhananjay Singh <dhananjay.singh@timesinternet.in>
2020-05-25 14:34:06 +0530

    Updates

---
+++ precache-manifest.b53e845380aaff4996382c4e77a46dcd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31086cf801fa010a45c46342ddb91a36",
+    "url": "/board/index.html"
+  },
+  {
+    "revision": "1ba285cac6cb38521bae",
+    "url": "/board/static/css/main.c9eb22c8.chunk.css"
+  },
+  {
+    "revision": "427c3c980064fdcc80dc",
+    "url": "/board/static/js/10.7bd3dc3f.chunk.js"
+  },
+  {
+    "revision": "83b315b4a937eac6752c",
+    "url": "/board/static/js/2.61426c10.chunk.js"
+  },
+  {
+    "revision": "d2807625e605fa134da29de723a799b1",
+    "url": "/board/static/js/2.61426c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc1cf6131a344658a24",
+    "url": "/board/static/js/3.9c39beb4.chunk.js"
+  },
+  {
+    "revision": "f41262f9bfbdd5aff314",
+    "url": "/board/static/js/4.6376c6ce.chunk.js"
+  },
+  {
+    "revision": "3b1b898214e1b5c6fbc6",
+    "url": "/board/static/js/5.5a87dd65.chunk.js"
+  },
+  {
+    "revision": "dc541e857573c791096c",
+    "url": "/board/static/js/6.3b0fe031.chunk.js"
+  },
+  {
+    "revision": "e4043501ea8a0ca973b9",
+    "url": "/board/static/js/7.dc5a7ea5.chunk.js"
+  },
+  {
+    "revision": "20e92a8785b9596ae825",
+    "url": "/board/static/js/8.27225fdb.chunk.js"
+  },
+  {
+    "revision": "476a50e5da79ad4b9f43",
+    "url": "/board/static/js/9.85292cf0.chunk.js"
+  },
+  {
+    "revision": "1ba285cac6cb38521bae",
+    "url": "/board/static/js/main.768679cf.chunk.js"
+  },
+  {
+    "revision": "0d87ad4998abc7cf4236",
+    "url": "/board/static/js/runtime-main.cd3dae01.js"
+  }
+]);
